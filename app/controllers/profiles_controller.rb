af3dbdff4b876bf5e26a99e3f7ef9d35c39b57b3
Amund Sole <amundbs91@gmail.com>
2017-05-28 14:05:19 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new  
   def new
     # Render a lank profile details form
+    @profile = Profile.new
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,3 @@
-class profile < ActiveRecord::Base
+class Profile < ActiveRecord::Base
   belongs_to :user  
 end
